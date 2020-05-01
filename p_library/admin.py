2a07fbb12ea31bb024f61abb5cb233082087f36e
Mikhail Yevseyev <m.yevseyev@gmail.com>
2020-05-01 13:33:35 +0600

    Initial commit

---
+++ p_library/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from django.contrib import admin
+from p_library.models import Book, Author, Redaction
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+
+    @staticmethod
+    def author_full_name(obj):
+        return obj.author.full_name
+
+    list_display = ('title','author_full_name',)
+    fields = ('ISBN','title', 'description', 'year_release', 'author', 'price', 'copy_count', 'redaction')
+
+@admin.register(Redaction)
+class RedactionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
